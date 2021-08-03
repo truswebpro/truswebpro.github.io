ba75edd4c053fa88cd43b8056ef1baa363d50e4a
truswebpro <87948010+truswebpro@users.noreply.github.com>
2021-08-03 20:17:30 +0300

    Add files via upload

---
+++ precache-manifest.8a1bca22d4dc90e0dec72238744428e8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d43c106c11689c58a95f06f520c15c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33f8052f3a5ac82ec5cf",
+    "url": "/static/css/main.e895f551.chunk.css"
+  },
+  {
+    "revision": "c937efa25eebc986e5eb",
+    "url": "/static/js/2.caf94737.chunk.js"
+  },
+  {
+    "revision": "81fbb27446145c382bc1dab229a0d054",
+    "url": "/static/js/2.caf94737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33f8052f3a5ac82ec5cf",
+    "url": "/static/js/main.fe9378ae.chunk.js"
+  },
+  {
+    "revision": "8b4699015b056ab4953a",
+    "url": "/static/js/runtime-main.d0868e4c.js"
+  },
+  {
+    "revision": "40ffa4ce74f8b826f3ca3e8f82da23bd",
+    "url": "/static/media/default_img_user.40ffa4ce.jpg"
+  },
+  {
+    "revision": "5bc723f8545ce7c528cf370c5aa54f67",
+    "url": "/static/media/loading.5bc723f8.gif"
+  }
+]);
